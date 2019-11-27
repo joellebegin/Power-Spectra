fixing scaling issues. fft being weird strikes again!

powerSpectrum.py
@@ -1,11 +1,12 @@
 import numpy as np
 from numpy.fft import fftn, fftshift
 
-def fourier(f):
-    return fftshift(fftn(fftshift(f)))
-
 class PowerSpectrum():
-    def __init__(self, field, bins = None, bin_w = 1, L = 300):
+
+    def fourier(self,f):
+        return fftshift(fftn(fftshift(f)))
+
+    def __init__(self, field, bins = None, L = 300):
         '''
         - field: field in fourier space
         - bins: array, if want to give bin edges in fourier space units
@@ -13,16 +14,21 @@ class PowerSpectrum():
         - L: real space length of box
         '''
         self.field = field
-        self.bins = bins
-        self.bin_w = bin_w
+        self.field_fourier = self.fourier(field)
+
+        if bins is not None:
+            self.bins = bins
+        else:
+            self.bins = np.logspace(np.log10(0.021), np.log10(3), num=13)
+
         self.L = L
         
         self.ndims = len(field.shape)
         self.n = field.shape[0] #number of pixels along one axis
-        self.abs_squared = np.abs(field)**2 #amplitude of field squared
+        self.abs_squared = np.abs(self.field_fourier)**2 #amplitude of field squared
         self.delta_k = 2*np.pi/self.L #kspace resolution of 1 pixel
         
-        self.survey_size = (L**self.ndims)#volume of box
+        self.survey_size = (self.n**self.ndims)#volume of box
     
     def r3_norm(self,rx,ry,rz):
         '''calculating length of each voxel's radial distance from origin'''
@@ -36,14 +42,13 @@ class PowerSpectrum():
         
         origin = self.n//2
         self.rmax = self.n - 0.5 - origin
-        
         if self.ndims == 2: 
             x,y = np.indices(self.field.shape)
             self.radii = np.hypot(x - origin, y - origin)*self.delta_k
             
         elif self.ndims == 3: 
             x,y,z = np.indices(self.field.shape)
-            self.radii = self.r3_norm(x-origin, y - origin, z - origin)*self.delta_k
+            self.radii = self.r3_norm(x-origin, y-origin, z-origin)*self.delta_k
         
         if del_squared:
                 self.abs_squared *= self.radii**3
@@ -61,7 +66,7 @@ class PowerSpectrum():
     def get_bin_ind(self):
         '''given the desired bin edges, determines the index of the last pixel
         going into this bin'''
-        bin_ind = [1]
+        bin_ind = [0]
         for bin_val in self.bins:
             val = np.argmax( self.r_sorted > bin_val)
             if val == 0: #ie bin_val > r_max
@@ -94,10 +99,7 @@ class PowerSpectrum():
         
         self.grid(del_squared) #sets up grid of radial distances
         
-        if self.bins is None: 
-            #use bin_w sparingly. I don't trust it
-            self.bins = np.arange(0,self.rmax + self.bin_w, self.bin_w)*self.delta_k
-        
+    
         self.sort() #sorting radii + field vals (increasing)
         self.get_bin_ind() #indices determing which elements go into bins
         self.average_bins() #computing average of bins
@@ -106,10 +108,9 @@ class PowerSpectrum():
         if del_squared:
             self.power *= (1/(2*np.pi**2))
         
-    def compute_pspec(self, del_squared = True, avg_k = True):
+    def compute_pspec(self, del_squared = True):
         self.p_spec(del_squared)
         return self.average_k, self.power
-
 
 def main():
     filename = input()
@@ -119,7 +120,7 @@ def main():
 
     bins =np.logspace(np.log10(0.021), np.log10(3), num=13)
 
-    k,delk = PowerSpectrum(fourier(box), bins = bins).compute_pspec()
+    k,delk = PowerSpectrum(box, bins = bins).compute_pspec()
     
     np.savetxt( "pspec_" + filename + '.csv', np.vstack((k,delk)), delimiter=',')
 

psToField.py
@@ -12,12 +12,9 @@ def fft_return_real(ndims,n):
                     field[i,j,k] = np.random.normal() + 1j*np.random.normal()
                     reflection = (-1*( np.array([i,j,k]) - half) + half)%n
                     field[tuple(reflection.astype(int))] = np.conj(field[i,j,k])
-        field[half,half,half] = 1
-        field[0,0,half] = 1
-        field[0,half,0] = 1
-        field[0,0,half] = 1
-        field[0,0,0] = 1
-
+                    if (reflection == np.array([i,j,k])).all:
+                        field[i,j,k] = np.real(field[i,j,k])
+    
     elif ndims ==2:
         field = np.zeros((n,n), dtype = complex)
         for i in range(n):
@@ -25,12 +22,8 @@ def fft_return_real(ndims,n):
                 field[i,j] = np.random.normal() + 1j*np.random.normal()
                 reflection = (-1*( np.array([i,j]) - half) + half)%n
                 field[tuple(reflection.astype(int))] = np.conj(field[i,j])
-
-        field[half,half] = 1
-        field[0,half] = 1
-        field[half,0] = 1
-        field[0,0] = 1
-
+                if (reflection == np.array([i,j])).all:
+                        field[i,j] = np.real(field[i,j])
     return field
 
 def r3_norm(rx,ry,rz):
@@ -67,9 +60,10 @@ def field(ps_function, shape, L):
     #distance from origin of each pixel/voxel
     r = radii(ndims,n,L)
     
+    survey_volume = L**ndims
     #each pixel will be drawn from gaussian distribution of this stdv
-    stdv_r = np.sqrt( (0.5)*(n**2)*ps_function(r) )
+    stdv_r = np.sqrt( (0.5)*survey_volume*ps_function(r) )
 
-    #scaling random gaussian dist according to given power spectrum
+    #scaling rand gaussian distribution according to given power spectrum
     kspace = rand_gauss * stdv_r 
     return np.real(fftshift(fftn(fftshift(kspace))))