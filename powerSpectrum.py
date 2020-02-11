d458d5a7246e201ac9a3ab4c5cf1b9c7e0db8f3e
joellebegin <joellebegin.6@gmail.com>
2020-02-10 23:05:27 -0500

    adding some nice comments. hmmmm documentation is satisfying

--- powerSpectrum.py
+++ powerSpectrum.py
@@ -6,10 +6,32 @@ class PowerSpectrum():
 
     def __init__(self, field, bins = None, L = 300, do_ft = True):
         '''
-        - field: field in fourier space
-        - bins: array, if want to give bin edges in fourier space units
-        - bin_w: else, specify bin width in pixel units
-        - L: real space length of box
+        Creates a power spectrum object, with methods that will output either a 
+        regular 1d power spectrum or a cylindrical power spectrum. 
+
+        Parameters
+        ----------
+        field: numpy ndarray
+            field for which to compute the power spectrum
+
+        bins: NoneType or 1darray
+            if not None, fourier space bid edges for isotropic averaging. 
+            the defauult is 13 logarithmically spaced bins as specified below
+
+        L: int or float
+            real space resolution of box in Mpc
+
+        do_ft: Bool
+            set to False if box is already in fourier space
+
+        Methods
+        -------
+        compute_pspec: 
+            returns the average k value going into each bin, and the power of 
+            that bin
+
+        cylindrical_pspec:
+            to be implemented
         '''
         self.field = field
         
@@ -31,20 +53,43 @@ class PowerSpectrum():
             self.bins = np.logspace(np.log10(0.021), np.log10(3), num=13)
 
         if do_ft:
-            self.field_fourier = self.fourier(field)
+            self.fourier()
         else:
             self.field_fourier = self.field
 
         self.abs_squared = np.abs(self.field_fourier)**2 
 
 
+    def fourier(self):
+        fourier_transform = fftshift(fftn(fftshift(self.field)))
+        scaled = fourier_transform*(self.delta_r**self.ndims)
+        self.field_fourier = scaled
+
     #======================= METHODS THAT OUTPUT BOXES ========================#
 
     def compute_pspec(self, del_squared = True, ignore_0 = False):
+        ''' computes the power spectrum. 
+
+        Parameters
+        ----------
+        del_squared: Bool
+            set to True if want the delta squared quantity rather than raw power
+
+        ignore_0: Bool
+            set to True if want to exclude the zero k vector in the averaging
+
+        Returns
+        -------
+        average_k: 1darray
+            the average value of k for the bins
+
+        power: 1darray
+            the power (or delta squared) of each bin
+        '''
         self.ignore_0 = ignore_0
         self.del_squared = del_squared
 
-        self.p_spec(del_squared, ignore_0)
+        self.p_spec()
         return self.average_k, self.power
 
 
@@ -53,34 +98,34 @@ class PowerSpectrum():
 
     #=============== METHODS RELATED TO VANILLA POWER SPECTRUM ================#
 
-    def p_spec(self, del_squared, ignore_0):
-            '''like the main method. Organizes stuff'''
+    def p_spec(self):
+            '''Main method of power spectrum compuation. Organizes functions.'''
 
             self.grid() #sets up grid of radial distances
             
-        
             self.sort() #sorting radii + field vals (increasing)
             self.get_bin_ind() #indices determing which elements go into bins
             self.average_bins() #computing average of bins
             self.power = self.field_bins/self.survey_size
             
-            if del_squared:
+            if self.del_squared:
                 self.power /= 2*np.pi**2
 
-    def fourier(self,f):
-        fourier_transform = fftshift(fftn(fftshift(f)))
-        scaled = fourier_transform*(self.delta_r**self.ndims)
-        return scaled
+    
 
     def r3_norm(self,rx,ry,rz): #can make this obsolete with np.norm giving axis =0
         '''calculating length of each voxel's radial distance from origin'''
         return np.sqrt(rx**2 + ry**2 + rz**2)
     
     def grid(self):
-        '''defines some useful variables
-        -r_max: maximum radial distance we consider
-        -radii: grid that contains radial distance of each pixel from origin, 
-        in kspace units'''
+        '''
+        Generates a fourier space grid with spacing set by box specs, and finds 
+        radial distance of each pixel from origin. Useful variables created:
+
+        radii: numpy ndarray 
+            grid that contains radial distance of each pixel from origin, 
+            in kspace units
+        '''
         
         origin = self.n//2
         self.rmax = self.n - 0.5 - origin
@@ -98,16 +143,17 @@ class PowerSpectrum():
                 self.abs_squared *= self.radii**3
                 
     def sort(self):
-        ''' sorts radii, and the field value corresponding to each radius.
-        sort_ind is here so as to not lose track of which radius corresponds
-        to which field value
+        ''' sorts radii, and the field value corresponding to each radius in 
+        ascending order. sort_ind is here so as to not lose track of which 
+        radius corresponds to which field value
         
         data is flattened in this step'''
+
         sort_ind = np.argsort(self.radii.flat)
         self.r_sorted = self.radii.flat[sort_ind]
         self.vals_sorted = self.abs_squared.flat[sort_ind] 
 
-        if self.ignore_0:
+        if self.ignore_0: #excluding zero vector
             self.r_sorted = self.r_sorted[1:]
             self.vals_sorted = self.vals_sorted[1:]
         
